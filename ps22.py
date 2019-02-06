f8945f882f6b44b9008ca895a7cb41315278ee11
Sherman Lim <shermz.lim.jh@gmail.com>
2019-02-06 13:47:47 +0800

    Added comments for ps22.py

--- ps22.py
+++ ps22.py
@@ -5,7 +5,7 @@
 
 # What is the total of all the name scores in the file?
 
-# obtaining names list from txt file 
+# obtaining names list from txt file & cleaning data 
 namesFile = open('ps22_names.txt')
 names = namesFile.read().split(',')
 names = [name.strip('"') for name in names]
@@ -14,20 +14,28 @@ namesFile.close()
 # sorting names in alphabetical order 
 names.sort()
 
-name_scores = 0 
+# function returns names score for list of names 
+def compute(names):
+    name_scores = 0 
 
-for index in range(len(names)):
-    
-    name = names[index]
+    # loops through list of names with index 
+    for index in range(len(names)):
+        name = names[index]
 
-    alphabetical_position = index + 1 
+        # obtain alphabetical position of name 
+        alphabetical_position = index + 1 
 
-    alphabetical_value = 0 
-    for char in name:
-        ascii_pos = ord(char)
-        value = ascii_pos - 64 
-        alphabetical_value += value 
+        # obtain alphabetical value 
+        alphabetical_value = 0 
+        for char in name:
+            ascii_pos = ord(char)
+            value = ascii_pos - 64 
+            alphabetical_value += value 
 
-    name_scores += (alphabetical_value*alphabetical_position)
+        # add name score to total name scores 
+        name_scores += (alphabetical_value*alphabetical_position)
 
-print(name_scores)    
+    return name_scores
+
+if __name__ == "__main__":
+    print(compute(names))    
