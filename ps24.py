3824da7c02bd468425aa2d00ea0c0cef63e7f340
Sherman Lim <shermz.lim.jh@gmail.com>
2019-02-08 15:54:10 +0800

    Refactored ps24

--- ps24.py
+++ ps24.py
@@ -8,7 +8,7 @@
 
 # What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 
-# analysis: possible permutations: 10! - 3628800 
+# analysis: possible permutations: num_digits! - 3628800 
 
 
 
@@ -25,39 +25,43 @@ def fact(n):
 # this algorithm finds the millionth lexicographic permutation by determining each digit of the permutation
 # from the first to last digit. As it is in lexicographic order, the digits can be determined 
 
-# digits available for use 
-digits = [i for i in range(10)]
+def compute(num_digits, nth_permutation):
+    # digits available for use 
+    digits = [i for i in range(num_digits)]
 
-# currently finding WHAT digit 
-currentFindingDigit = 1 
+    # currently finding WHAT digit 
+    currentFindingDigit = 1 
 
-# records digit of millionth_permutation
-millionth_permutation = ''
+    # records digit of answer
+    answer = ''
 
-# remaining permutations left to reach 1 millionth lexicographic permutation 
-remaining = 999999
+    # remaining permutations left to reach nth lexicographic permutation 
+    remaining = nth_permutation - 1 
 
 
 
-for i in range(10):
-    possiblePermsForDigit = fact(10 - currentFindingDigit)
-    
-    digitIndex = remaining//possiblePermsForDigit
-    digit = digits[digitIndex]
+    for i in range(num_digits):
+        possiblePermsForDigit = fact(num_digits - currentFindingDigit)
+        
+        digitIndex = remaining//possiblePermsForDigit
+        digit = digits[digitIndex]
 
-    # example:
-    # lexicographical permutations:
-    # first digit: 0 - 362880 permutations 
-    # first digit: 1 - 362880 p 
-    # first digit: 2 - 362880 p (1 millionth permutation contained inside) Therefore, first digit is 2 
+        # example:
+        # lexicographical permutations:
+        # first digit: 0 - 362880 permutations 
+        # first digit: 1 - 362880 p 
+        # first digit: 2 - 362880 p (1 millionth permutation contained inside) Therefore, first digit is 2 
 
-    millionth_permutation += str(digit)
-    # when digit is used up, it will no longer be taken into consideration 
-    digits.remove(digit)
+        answer += str(digit)
+        # when digit is used up, it will no longer be taken into consideration 
+        digits.remove(digit)
 
-    # moves on to next digit 
-    currentFindingDigit += 1 
+        # moves on to next digit 
+        currentFindingDigit += 1 
 
-    remaining -= (possiblePermsForDigit*digitIndex)
+        remaining -= (possiblePermsForDigit*digitIndex)
 
-print(millionth_permutation)    
+    return answer    
+
+if __name__ == "__main__":
+    print(compute(10, 1000000))
