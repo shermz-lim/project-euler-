4ab0136da80b25568198f71544abc80877b45057
Sherman Lim <shermz.lim.jh@gmail.com>
2019-02-16 09:58:17 +0800

    Added ps30

--- ps30.py
+++ ps30.py
@@ -2,13 +2,17 @@
 
 # Find the sum of all the numbers that can be written as the sum of fifth powers of their digits. 
 
-valid_numbers = []
+def compute():
+    valid_numbers = []
 
-for number in range(10, 295245):
-    total = 0 
-    for digit in str(number):
-        total += (int(digit))**5
-    if total == number:
-        valid_numbers.append(number)
+    for number in range(10, 300000):
+        total = 0 
+        for digit in str(number):
+            total += (int(digit))**5
+        if total == number:
+            valid_numbers.append(number)
 
-print(sum(valid_numbers))        
+    return sum(valid_numbers)   
+
+if __name__ == "__main__":
+    print(compute())    
